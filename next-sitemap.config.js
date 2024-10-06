8f59686e8d5e5de9514b4e5ba5c37286cb720900
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-10-07 01:32:30 +0530

    ..

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@
 module.exports = {
     siteUrl: 'http://localhost:3000', // Apne site URL se replace karein
     generateRobotsTxt: true, // Optional: agar aap robots.txt chahte hain
+    sitemapSize: 7000,
   };
   
